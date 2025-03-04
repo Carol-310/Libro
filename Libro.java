df61eac5306719dfe4bd35ce947421b5cbecdfe8
carolina <carolmunoz1516@gmail.com>
2025-03-03 20:51:18 -0500

    First commit

--- Libro.java
+++ Libro.java
@@ -1,25 +1,29 @@
 import java.util.Scanner;
 
 public class Libro {
+
     private String titulo;
     private String autor;
     private int numEjemplares;
-    private int ejemplaresPrestados;
+    private int numEjemplaresPrestados;
 
+    
     public Libro() {
         this.titulo = "";
         this.autor = "";
         this.numEjemplares = 0;
-        this.ejemplaresPrestados = 0;
+        this.numEjemplaresPrestados = 0;
     }
 
-    public Libro(String titulo, String autor, int numEjemplares, int ejemplaresPrestados) {
+    
+    public Libro(String titulo, String autor, int numEjemplares, int numEjemplaresPrestados) {
         this.titulo = titulo;
         this.autor = autor;
         this.numEjemplares = numEjemplares;
-        this.ejemplaresPrestados = ejemplaresPrestados;
+        this.numEjemplaresPrestados = numEjemplaresPrestados;
     }
 
+    
     public String getTitulo() {
         return titulo;
     }
@@ -44,32 +48,81 @@ public class Libro {
         this.numEjemplares = numEjemplares;
     }
 
-    public int getEjemplaresPrestados() {
-        return ejemplaresPrestados;
+    public int getNumEjemplaresPrestados() {
+        return numEjemplaresPrestados;
     }
 
-    public void setEjemplaresPrestados(int ejemplaresPrestados) {
-        this.ejemplaresPrestados = ejemplaresPrestados;
+    public void setNumEjemplaresPrestados(int numEjemplaresPrestados) {
+        this.numEjemplaresPrestados = numEjemplaresPrestados;
     }
 
-    public boolean prestar() {
-        if (numEjemplares > ejemplaresPrestados) {
-            ejemplaresPrestados++;
+    
+    public boolean prestamo() {
+        if (numEjemplaresPrestados < numEjemplares) {
+            numEjemplaresPrestados++;
             return true;
         } else {
             return false;
         }
     }
 
-    public boolean devolver() {
-        if (ejemplaresPrestados > 0) {
-            ejemplaresPrestados--;
+    
+    public boolean devolucion() {
+        if (numEjemplaresPrestados > 0) {
+            numEjemplaresPrestados--;
             return true;
         } else {
             return false;
         }
     }
+
+    
+    @Override
     public String toString() {
-        return "Título: " + titulo + "\nAutor: " + autor + "\nEjemplares: " + numEjemplares + "\nPrestados: " + ejemplaresPrestados;
+        return "Título: " + titulo +
+               "\nAutor: " + autor +
+               "\nNúmero de ejemplares: " + numEjemplares +
+               "\nNúmero de ejemplares prestados: " + numEjemplaresPrestados;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        
+        Libro libro1 = new Libro("chocolate para el alma", "adam samdler", 8, 0);
+
+        
+        Libro libro2 = new Libro();
+        System.out.println("Ingrese título del libro:");
+        libro2.setTitulo(scanner.nextLine());
+        System.out.println("Ingrese autor del libro:");
+        libro2.setAutor(scanner.nextLine());
+        System.out.println("Ingrese número de ejemplares:");
+        libro2.setNumEjemplares(scanner.nextInt());
+        System.out.println("Ingrese número de ejemplares prestados:");
+        libro2.setNumEjemplaresPrestados(scanner.nextInt());
+        scanner.close(); 
+
+        
+        System.out.println("\nLibro 1:");
+        System.out.println(libro1.toString());
+        System.out.println("\nLibro 2:");
+        System.out.println(libro2.toString());
+
+        System.out.println("\nPréstamo de libro 1: " + libro1.prestamo());
+        System.out.println("Préstamo de libro 2: " + libro2.prestamo());
+
+        System.out.println("\nLibro 1 después del préstamo:");
+        System.out.println(libro1.toString());
+        System.out.println("\nLibro 2 después del préstamo:");
+        System.out.println(libro2.toString());
+
+        System.out.println("\nDevolución de libro 1: " + libro1.devolucion());
+        System.out.println("Devolución de libro 2: " + libro2.devolucion());
+
+        System.out.println("\nLibro 1 después de la devolución:");
+        System.out.println(libro1.toString());
+        System.out.println("\nLibro 2 después de la devolución:");
+        System.out.println(libro2.toString());
     }
 }
